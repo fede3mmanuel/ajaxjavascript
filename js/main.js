8b74f45b4919f3e7e7c5856b40d77f08f89fb282
Fede <fede.fede.website>
2021-06-13 16:35:11 -0500

    getting the first part of the json/the browser knows that is a json file

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.js
 ourRequest.onload = function () {
     // console.log(ourRequest.responseText);
     // With the folowing the browser thinks that is text
-    let ourData = ourRequest.responseText;
+    // let ourData = ourRequest.responseText;
+    // With the folowing the browser knows that is a json file
+    let ourData = JSON.parse(ourRequest.responseText)
     console.log(ourData[0]);
 };
 
