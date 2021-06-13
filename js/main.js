1e2a82b9586bdba67a4f8f420f0d5353f58a9fe0
Fede <fede.fede.website>
2021-06-13 16:32:56 -0500

    getting the first character

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
 
 ourRequest.onload = function () {
-    console.log(ourRequest.responseText);
+    // console.log(ourRequest.responseText);
+    // With the folowing the browser thinks that is text
+    let ourData = ourRequest.responseText;
+    console.log(ourData[0]);
 };
 
 ourRequest.send();
