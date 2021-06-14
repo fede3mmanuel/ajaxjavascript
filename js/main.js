632641fcf4572e2bbae130d72868bd9e26c467f7
Fede <fede.fede.website>
2021-06-14 13:01:41 -0500

    every time the button is pressed it puts the data from the json file

--- js/main.js
+++ js/main.js
@@ -7,18 +7,27 @@ btn.addEventListener("click", function() {
     ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
 
     ourRequest.onload = function () {
+    
         // console.log(ourRequest.responseText);
         // With the folowing the browser thinks that is text
         // let ourData = ourRequest.responseText;
         // With the folowing the browser knows that is a json file
         let ourData = JSON.parse(ourRequest.responseText)
         renderHTML(ourData);
-};
+    
+    };
 
-ourRequest.send();
+    ourRequest.send();
 });
 
 function renderHTML(data) {
-    let htmlString = "this is a test";
+    let htmlString = "";
+
+    for (let i = 0; i < data.length; i++) {
+        
+        htmlString += "<p>" + data[i].name + " is a" + data[i].species + ".</p>";
+        
+    }
+    
     animalContainer.insertAdjacentHTML('beforeend', htmlString)
 }
