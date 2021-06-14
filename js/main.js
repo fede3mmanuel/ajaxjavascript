59c56669e733ea106a98cd7812837f3828bd36f5
Fede <fede.fede.website>
2021-06-14 12:41:14 -0500

    the ajax function is executed when the button is pressed

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+var btn = document.getElementById("btn");
 
-ourRequest.onload = function () {
-    // console.log(ourRequest.responseText);
-    // With the folowing the browser thinks that is text
-    // let ourData = ourRequest.responseText;
-    // With the folowing the browser knows that is a json file
-    let ourData = JSON.parse(ourRequest.responseText)
-    console.log(ourData[0]);
+btn.addEventListener("click", function() {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+
+    ourRequest.onload = function () {
+        // console.log(ourRequest.responseText);
+        // With the folowing the browser thinks that is text
+        // let ourData = ourRequest.responseText;
+        // With the folowing the browser knows that is a json file
+        let ourData = JSON.parse(ourRequest.responseText)
+        console.log(ourData[0]);
 };
 
 ourRequest.send();
+});
