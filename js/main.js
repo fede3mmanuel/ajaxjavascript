82fbd676804de80f1c09f936d8177dfd486744e1
Fede <fede.fede.website>
2021-06-15 12:20:32 -0500

    dissapear the button when the number 4 is reached

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
+let pageCounter = 1;
+
 var animalContainer = document.getElementById("animal-info");
 
 var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    ourRequest.open('GET', `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`);
 
     ourRequest.onload = function () {
     
@@ -18,6 +20,11 @@ btn.addEventListener("click", function() {
     };
 
     ourRequest.send();
+    pageCounter++;
+
+    if (pageCounter > 3) {
+        btn.classList.add("hide-me");        
+    }
 });
 
 function renderHTML(data) {
